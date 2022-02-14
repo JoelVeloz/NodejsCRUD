4976c0e5f60cf36321e22beae6f8e962d293dfd8
Joel Veloz <joevpazm@espol.edu.ec>
2022-02-14 00:46:58 -0300

    credentials

--- database/db.js
+++ database/db.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql')
+require('dotenv').config()
+
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const database = process.env.DB_DATABASE;
+const user = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
 
 const conexion = mysql.createConnection({
-    host: 'localhost',
-    // port: '8080',               
-    user: 'root',
-    password: '',
-    database: 'crudnodejs'
+    host: host,
+    user: user,
+    port: port,
+    password: password,
+    database: database,
 })
 conexion.connect((error) => {
     if (error) {
